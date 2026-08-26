d54fe63ca1fa1e719bbab3a24716ff0bef1fd5ef
Prgm-code <pgomez.meneses@gmail.com>
2026-08-25 21:42:18 -0400

    initial

---
+++ src/main/java/cl/prgm/portaltrip/PortaltripApplication.java
@@ -0,0 +1,13 @@
+package cl.prgm.portaltrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortaltripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortaltripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/prgm/portaltrip/PortaltripApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.prgm.portaltrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortaltripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
